b8d5b2dfa20b866243af472184de46cc7918608a
alpha-abhii <alpha20abhi@gmail.com>
2024-06-04 07:30:28 +0530

    add user model and user validation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,10 +1,26 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body)
-    return res.status(200).json({
-        msg: "Got your data"
-    })
+    // console.log(req.body)
+    // return res.status(200).json({
+    //     msg: "Got your data"
+    // })
+
+    const {name,email,password} = req.body;
+
+    if(!name || !email || !password){
+        const error = createHttpError(400, "All fiels are required");
+        return next(error);
+    }
+
+    const user = await userModel.findOne({email});
+
+    if(user) {
+        const error = createHttpError(400,"User already exists with this email.")
+        return next(error);
+    }
 }
 
 export { createUser };
